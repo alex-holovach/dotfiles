596e02e3a5556d97159b3b3088f0bbfbbc8ae972
Alex Holovach <alex.holovach@prove.com>
2024-10-12 14:16:32 -0500

    nvim config

---
+++ .config/nvim/lua/plugins/init.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      -- add any options here
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function() end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
