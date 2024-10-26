09e0c7c3f08d943244953bef4666bb66029ba09b
Alex Holovach <holovach.oleksandr@gmail.com>
2024-10-26 12:57:06 -0500

    stow nvim config

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+      background = "mocha",
+    },
+  },
+}
