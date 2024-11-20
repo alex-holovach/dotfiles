b07b75ceed502a12ed6288a0e32645c72a28b198
Alex Holovach <alex.holovach@prove.com>
2024-11-20 07:28:19 -0600

    ignore system shorcuts

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<D-M-C-S-s>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<D-M-C-S-s>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<D-M-C-S-s>", "<Nop>", { noremap = true })
+
+vim.keymap.set("n", "<D-M-C-S-a>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<D-M-C-S-a>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<D-M-C-S-a>", "<Nop>", { noremap = true })
+
+vim.keymap.set("n", "<D-M-C-S-t>", "<Nop>", { noremap = true })
+vim.keymap.set("i", "<D-M-C-S-t>", "<Nop>", { noremap = true })
+vim.keymap.set("v", "<D-M-C-S-t>", "<Nop>", { noremap = true })
