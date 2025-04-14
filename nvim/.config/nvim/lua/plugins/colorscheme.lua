2b2262383355568f470c250b0e5842ed000595b3
Alex Holovach <alex.holovach@kubiks.ai>
2025-04-14 11:26:05 -0500

    update themes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,27 @@
+-- return {
+--   -- add gruvbox
+--   -- { "ellisonleao/gruvbox.nvim" },
+--   { "catppuccin/nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "catppuccin",
+--       background = "mocha",
+--     },
+--   },
+-- }
+--
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
-  { "catppuccin/nvim" },
-
-  -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-      background = "mocha",
-    },
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard" -- darkest background
+      vim.g.gruvbox_material_foreground = "material" -- richer, modern fg
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.cmd("colorscheme gruvbox-material")
+    end,
   },
 }
